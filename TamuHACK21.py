895082ca4f37f9babce4965f187ddf3247fb4190
imasabo2k18 <imasabo2k18@tamu.edu>
2021-01-31 00:19:11 -0600

    added user

--- TamuHACK21.py
+++ TamuHACK21.py
@@ -1,4 +1,4 @@
-from user import User
+# from user import User
 import  random
 import csv
 print('Welcome To Finance Pro')
@@ -27,6 +27,21 @@ while try_again:
     else:
         break
 
+
+def error_prompt_():
+    loop = True
+    while loop:
+        error_prompt = input('Would you like to try again? (Enter yes/no): ')
+        if error_prompt.lower() != 'no' and error_prompt.lower() != 'yes':
+            print('Wrong input.')
+            continue
+        elif error_prompt.lower() == 'no':
+            loop = False
+            exit()
+        elif error_prompt.lower() == 'yes':
+            break
+        else:
+            continue
 
 def login():
     user_name = input('Please enter user name: ')
@@ -62,22 +77,71 @@ def login():
 
 def signup():
     with open("user_database.csv", "a") as file:
-        user_name = input("Enter name: ")
-        user_last = input("Enter Last name: ")
+        user_name = input("Enter first name: ")
+        user_last = input("Enter last name: ")
         user_password = input("Enter your Password: ")
-        user_dob = (input("Enter your DOB(EX:01/19/1997): "))
-        user_ssn = int(input("Enter your SSN: "))
-        user_i_d = int(input("Enter your id: "))
-        user_email = input("Enter your email: ")
-        user_acct_number = random.randint(100, 300)
-        storage = [user_name,user_last,user_password,user_dob,str(user_ssn),str(user_i_d),str(user_acct_number), user_email, user_password]
+        dob = True
+        while dob:
+            user_dob = input("Enter your DOB(EX:01/19/1997): ")
+            month = 0
+            day = 0
+            year = 0
+            try:
+                month = int(user_dob[0: 2])
+                day = int(user_dob[3: 5])
+                year = int(user_dob[6:])
+            except:
+                print('Wrong input for date of birth. Please input in the correct format.')
+                error_prompt_()
+            if month <= 12 and day <= 31 and 2010 >= year >= 1900 \
+                    and user_dob[2] == '/' and user_dob[5] == '/':
+                break
+            else:
+                continue
+        while_loop = True
+        while while_loop:
+            user_ssn = input("Enter your SSN (9-digit number): ")
+            if len(user_ssn) == 9:
+                try:
+                    ssn = int(user_ssn)
+                    break
+                except:
+                    print('Wrong input for Social Security Number. Acceptable input is a 9-digit number.')
+                    error_prompt_()
+            else:
+                error_prompt_()
+        while_loop = True
+        while while_loop:
+            dl = input('Please enter your state drivers number (8-digit number): ')
+            if len(dl) == 8:
+                try:
+                    drivers_l = int(dl)
+                    break
+                except:
+                    print('Wrong input for Drivers License. Acceptable input is an 8-digit number.')
+                    error_prompt_()
+        loop = True
+        while loop:
+            user_email = input("Enter your email: ")
+            for character in user_email:
+                if character == '@':
+                    loop = False
+                    break
+                else:
+                    continue
+            if loop:
+                print('Wrong input for email. Valid input must include @ sign.')
+                error_prompt_()
+
+        user_acct_number = random.randint(100000, 900000)
+        storage = [user_name, user_last, user_password, user_dob, str(ssn), str(drivers_l), str(user_acct_number),
+                   user_email]
         num = 0
         for item in storage:
             file.write(item)
             if num < len(storage)-1:
                 file.write(',')
                 num += 1
-        file.write("\n")
 
 
 if sign_up_log_in.lower() == 'l':
